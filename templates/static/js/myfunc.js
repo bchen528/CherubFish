822afb5e3187b1df56c288636eb9fb9666bc6d53
Syssos <paral.cody98@gmail.com>
2018-08-31 13:55:57 -0700

    js function for url bar

---
+++ templates/static/js/myfunc.js
@@ -0,0 +1,8 @@
+function dropDownfunc() {
+    var x = document.getElementById("Url_response");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
