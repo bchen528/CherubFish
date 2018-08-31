1cee2f4669ee2836e4ac27decb60eae87d43af8a
gjdame <gregoryjdame@gmail.com>
2018-08-31 19:29:38 +0000

    start update

--- start.py
+++ start.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
 from app import app, db
+import sqlite3
+import requests
+from flask import Flask, request, abort, render_template
+from hashlib import md5
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def create_url():
 
-    url = request.form.get('url')
-    response = requests.get(url)
-    if response.status_code != 200:
-        abort(400, "Invalid Website")
-    short_url = md5(url).encode().hexdigest()[-6:]
-    with sqlite3.connect('users.db') as conn:
-        cursor = conn.cursor()
-        insert_row = """
-            INSERT INTO Url (URL, SHORT_URL)
-                  VALUES ('%s, %s')
-            """%(url, short_url)
-        result_cursor = cursor.execute(insert_row)
-    return render_template('test.html', short_url)
-return render_template('test.html')
+    if request.method == 'POST':
+        url = request.form.get('url')
+        #    response = requests.get(url)
+        #   if response.status_code != 200:
+        #      abort(400, "Invalid Website")
+        short_url = md5(url.encode()).hexdigest()[-6:]
+        with sqlite3.connect('app.db') as conn:
+            cursor = conn.cursor()
+            insert_row = """
+               INSERT INTO Url (URL, SHORT_URL)
+                   VALUES ('%s, %s')
+               """%(url, short_url)
+            result_cursor = cursor.execute(insert_row)
+        return render_template('/test.html', short_url)
+    return render_template('/test.html')
 
-@app.route('/<short_url>')
+'''@app.route('/<short_url>')
 def redirect(short_url):
     with sqlite3.connect('urls.db') as conn:
         cursor = conn.cursor()
         select_row = """
                 SELECT URL FROM URL
                     WHERE short_url = short_url
-                """%(decoded_string)
+                """%(short_url)
     result_cursor = cursor.execute(select_row)
     try:
         redirect_url = result_cursor.fetchone()[0]
@@ -34,6 +39,6 @@ def redirect(short_url):
         print('YOU FUCKED UP')
 
     return redirect(redirect_url)
-
+'''
 if __name__ == "__main__":
-    app.run(debug==True)
+    app.run(host="0.0.0.0", port=5000)
