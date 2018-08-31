f8484c429fd3d5ac6a92963946831af264079c79
gjdame <gregoryjdame@gmail.com>
2018-08-31 18:41:20 +0000

    add db and start

--- app/models.py
+++ app/models.py
@@ -8,3 +8,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
+
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    long_url = db.Column(db.String(120), index=True, unique=True)
+    short_url = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Url {}, {}>'.format(self.long_url, self.short_url)
--- start.py
+++ start.py
@@ -1,2 +1,39 @@
 #!/usr/bin/python3
 from app import app, db
+
+@app.route('/', methods=['POST'])
+def create_url():
+
+    url = request.form.get('url')
+    response = requests.get(url)
+    if response.status_code != 200:
+        abort(400, "Invalid Website")
+    short_url = md5(url).encode().hexdigest()[-6:]
+    with sqlite3.connect('users.db') as conn:
+        cursor = conn.cursor()
+        insert_row = """
+            INSERT INTO Url (URL, SHORT_URL)
+                  VALUES ('%s, %s')
+            """%(url, short_url)
+        result_cursor = cursor.execute(insert_row)
+    return render_template('test.html', short_url)
+return render_template('test.html')
+
+@app.route('/<short_url>')
+def redirect(short_url):
+    with sqlite3.connect('urls.db') as conn:
+        cursor = conn.cursor()
+        select_row = """
+                SELECT URL FROM URL
+                    WHERE short_url = short_url
+                """%(decoded_string)
+    result_cursor = cursor.execute(select_row)
+    try:
+        redirect_url = result_cursor.fetchone()[0]
+    except:
+        print('YOU FUCKED UP')
+
+    return redirect(redirect_url)
+
+if __name__ == "__main__":
+    app.run(debug==True)
