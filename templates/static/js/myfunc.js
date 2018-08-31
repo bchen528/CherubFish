17214a9cd80329e09d165df92b7b4ddfa018f637
Syssos <paral.cody98@gmail.com>
2018-08-31 14:11:19 -0700

    updated functionality for response

--- templates/static/js/myfunc.js
+++ templates/static/js/myfunc.js
@@ -2,7 +2,12 @@ function dropDownfunc() {
     var x = document.getElementById("Url_response");
     if (x.style.display === "none" || x.style.display === "") {
         x.style.display = "block";
-    } else {
-        x.style.display = "none";
+    }
+}
+
+function dropDownfunc2() {
+    var x = document.getElementById("Url_response2");
+    if (x.style.display === "none" || x.style.display === "") {
+        x.style.display = "block";
     }
 }
