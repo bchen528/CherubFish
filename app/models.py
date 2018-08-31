d4b6a1e65d4a68d8d5120e90d7d9b7424c2cf7d7
Mikaela Gurney <354@holbertonschool.com>
2018-08-31 11:13:07 -0700

    Add sqlite integration with Migrations

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import db
+from flask import current_app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
